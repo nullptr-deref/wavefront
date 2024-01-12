bc3844b38299ad89760241dc8bda1d479db15652
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2024-01-12 05:04:13 +0300

    Improve test a bit (visually, not functionally)

--- tests/parse.c
+++ tests/parse.c
@@ -2,9 +2,10 @@
 
 #include <stdio.h>
 
-const char *files[2] = {
+const char *files[3] = {
     "./tests/male.obj",
-    "./tests/mustang.obj"
+    "./tests/mustang.obj",
+    "./tests/sphere.obj"
 };
 
 int main() {
@@ -14,7 +15,10 @@ int main() {
         return 1;
     }
     WavefrontGeometry *g = wavefront_fread(f);
-    const size_t vc = printf("Parsed %ld vertices and %ld faces from %s\n", g->vertices_count, g->faces_count, files[1]);
+    printf("Parsed %ld objects containing %ld vertices, %ld faces from %s\n", g->objects_count, g->vertices_count, g->faces_count, files[1]);
+    for (size_t i = 0; i < g->objects_count; i++) {
+        printf("Object %ld:%s\n\toffset = %ld;\n\tlen = %ld\n", i, g->objects[i].name, g->objects[i].owned_vertices.offset, g->objects[i].owned_vertices.len);
+    }
     fclose(f);
     wavefront_geometry_free(g);
 
