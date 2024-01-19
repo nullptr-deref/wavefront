db3fe968427e5dac9e1d66aac29b2630448fd1b2
Rostislav Tolushkin <rostislav.tolushkin@gmail.com>
2024-01-19 05:09:41 +0300

    Fix type name conflict + code alignment

--- include/geometry.h
+++ include/geometry.h
@@ -6,7 +6,7 @@
 #include <stddef.h>
 
 typedef size_t idx_t;
-typedef size_t id_t;
+typedef size_t wavefront_id_t;
 
 typedef struct {
     idx_t  *vertices_data; // Array stores [v/vt/vn] tightly.
@@ -14,17 +14,17 @@ typedef struct {
 } Face;
 
 typedef struct {
-    char *name;
-    id_t id;
-    Range polygons;
+    char          *name;
+    wavefront_id_t id;
+    Range          polygons;
 } Group;
 
 typedef struct {
-    id_t     id;
-    char    *name;
-    Group   *groups;
-    size_t   group_count;
-    Range    owned_vertices;
+    wavefront_id_t id;
+    char          *name;
+    Group         *groups;
+    size_t         group_count;
+    Range          owned_vertices;
 } Object;
 
 typedef struct {
